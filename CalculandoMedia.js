fc9548b8a36eb4222cc2ef50944da5b70e725b93
Kelvin Harlan <kelvinharlandev@gmmail.com>
2022-06-25 10:45:30 -0300

    Retornando a media entre 4 notas

---
+++ CalculandoMedia.js
@@ -0,0 +1,7 @@
+/*Faça um Programa que peça as 4 notas bimestrais e mostre a média.*/
+
+function MostraMedia(nota1, nota2, nota3, nota4){
+    console.log((nota1+nota2+nota3+nota4)/4)
+}
+
+MostraMedia(8,8,8,8)
